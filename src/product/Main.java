ddb396905223cc65cb2de3f7fbfa65571aeabdd7
denizaliyev1605 <denizaliyev4012@gmail.com>
2026-04-08 20:31:34 +0400

    Sabirin projectit

--- src/product/Main.java
+++ src/product/Main.java
@@ -656,6 +656,8 @@ public class Main {
         System.out.println("Mixed list size after: " + mixedList.size());
         System.out.println("Last item: " + mixedList.get(mixedList.size() - 1).sku);
         System.out.println("Hello world");
+        System.out.println("Hello world");
+
     }
 }
 
